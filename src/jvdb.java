d8396f13ec790ea5c14c3a2d5beef8a7c629ff99
Petar Stanev <petarstanevtues@gmail.com>
2014-02-25 23:00:19 +0200

    Added Create,insert,select and drop.

--- src/jvdb.java
+++ src/jvdb.java
@@ -1,11 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class jvdb {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test git");
-		System.out.println("test git");
-		System.out.println("test git");
-	}
+	public static void main(String[] args) throws ClassNotFoundException,
+			SQLException {
+		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		Connection con = DriverManager
+				.getConnection("jdbc:derby:wombat;create=true");
+		Statement stmt = con.createStatement();
+		stmt.execute("CREATE TABLE users (username VARCHAR(10), password VARCHAR(10))");
 
+		stmt.execute("INSERT INTO users (username, password) VALUES ('asd','123456')");
+		//INSERT INTO Employees (FirstName, SecondName) VALUES ('Nenko', 	'Tabakov');
+		stmt = con.createStatement();
+
+		ResultSet rs = stmt.executeQuery("SELECT password,username FROM users");
+		while (rs.next()) {
+			String user = rs.getString("username");
+			String pass = rs.getString("password");
+			System.out.print(user);
+			System.out.println(pass);
+		}
+		stmt.execute("DROP TABLE users");
+	}
 }
