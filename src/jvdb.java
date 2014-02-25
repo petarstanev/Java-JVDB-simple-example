afcdc2335646c87f31765d3d7e2e8ac75ed50365
Petar Stanev <petarstanevtues@gmail.com>
2014-02-25 23:20:55 +0200

    Added real table with some users with autoincrement id

--- src/jvdb.java
+++ src/jvdb.java
@@ -12,18 +12,23 @@ public class jvdb {
 		Connection con = DriverManager
 				.getConnection("jdbc:derby:wombat;create=true");
 		Statement stmt = con.createStatement();
-		stmt.execute("CREATE TABLE users (username VARCHAR(10), password VARCHAR(10))");
+		stmt.execute("CREATE TABLE users (id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1,INCREMENT BY 1), username VARCHAR(10), password VARCHAR(10))");
 
-		stmt.execute("INSERT INTO users (username, password) VALUES ('asd','123456')");
+		stmt.execute("INSERT INTO users (username, password) VALUES ('user0','123456')");
+		stmt.execute("INSERT INTO users (username, password) VALUES ('user1','123123')");
+		stmt.execute("INSERT INTO users (username, password) VALUES ('user2','121212')");
+		stmt.execute("INSERT INTO users (username, password) VALUES ('user3','654321')");
+		stmt.execute("INSERT INTO users (username, password) VALUES ('user4','111111')");
 		//INSERT INTO Employees (FirstName, SecondName) VALUES ('Nenko', 	'Tabakov');
 		stmt = con.createStatement();
 
-		ResultSet rs = stmt.executeQuery("SELECT password,username FROM users");
+		ResultSet rs = stmt.executeQuery("SELECT * FROM users");
 		while (rs.next()) {
+			String id = rs.getString("id");
 			String user = rs.getString("username");
 			String pass = rs.getString("password");
-			System.out.print(user);
-			System.out.println(pass);
+			System.out.println(id + " " + user + " " + pass);
+			//System.out.println(pass);
 		}
 		stmt.execute("DROP TABLE users");
 	}
